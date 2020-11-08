28e7a9524883943a1edebbb0dfa01ba8ec799d28
ilhamizaki <72953685+ilhamizaki@users.noreply.github.com>
2020-11-08 11:51:04 +0700

    Add files via upload

---
+++ src/com/ilham/Tabungan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ilham;
+
+/**
+ *
+ * @author
+ * NAMA     : Ilham zaki
+ * Kelas    : IF 1
+ * NIM      : 10119006
+ * Deskripsi Program : class Tabungan
+ */
+public class Tabungan {
+    private final int saldo;
+    
+    public Tabungan(int saldo) {
+        this.saldo = saldo;
+    }
+    
+    public int ambilUang(int jumlah) {
+        return saldo - jumlah;
+    }
+    
+}
